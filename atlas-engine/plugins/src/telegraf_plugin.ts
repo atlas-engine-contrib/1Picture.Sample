e2f92df2d2c71efe2482482f524d0306b4ecfe07
Robin <robin.lenz@5minds.de>
2021-02-09 08:26:04 +0100

    :recycle: update telegraf_plugin

--- atlas-engine/plugins/src/telegraf_plugin.ts
+++ atlas-engine/plugins/src/telegraf_plugin.ts
@@ -1,34 +1,38 @@
-import {registerEventMiddleware} from '@atlas-engine/fullstack_server';
-import Axios, {AxiosRequestConfig} from "../node_modules/axios";
+import {AtlasEngine, AtlasEngineEvent} from "@atlas-engine/atlas_engine_sdk";
+import Axios, {AxiosRequestConfig} from "axios";
 
 const axiosRequestConfig: AxiosRequestConfig = {headers: {'X-Requested-With': 'XMLHttpRequest'}}
 
 const TELEGRAF_HTTP_LISTENER: string = "http://host.docker.internal:8080/telegraf"
 
+export async function onLoad(engine: AtlasEngine): Promise<void> {
 
-registerEventMiddleware(async (atlasEngineEvent) => {
+  await engine.registerEventMiddleware(async (atlasEngineEvent: AtlasEngineEvent) => {
 
-  const data = {
-    eventType: atlasEngineEvent.eventType,
-    timestamp: atlasEngineEvent.timestamp,
-    processModelId: atlasEngineEvent.processModelId,
-    flowNodeId: atlasEngineEvent.flowNodeId,
-    flowNodeInstanceId: atlasEngineEvent.flowNodeInstanceId,
-    flowNodeType: atlasEngineEvent. flowNodeType,
-    processInstanceId: atlasEngineEvent.processInstanceId,
-    correlationId: atlasEngineEvent.correlationId,
-    logLevel: atlasEngineEvent. logLevel,
-    tokenPayload: JSON.stringify(atlasEngineEvent.tokenPayload) ?? 'undefined'
-  }
+    const data = {
+      eventType: atlasEngineEvent.eventType,
+      timestamp: atlasEngineEvent.timestamp,
+      processModelId: atlasEngineEvent.processModelId,
+      flowNodeId: atlasEngineEvent.flowNodeId,
+      flowNodeInstanceId: atlasEngineEvent.flowNodeInstanceId,
+      flowNodeType: atlasEngineEvent.flowNodeType,
+      processInstanceId: atlasEngineEvent.processInstanceId,
+      correlationId: atlasEngineEvent.correlationId,
+      logLevel: atlasEngineEvent.logLevel,
+      tokenPayload: JSON.stringify(atlasEngineEvent.tokenPayload) ?? 'undefined'
+    }
 
-  Axios.post(TELEGRAF_HTTP_LISTENER, data, axiosRequestConfig)
-  .then(res => {
-    console.log('Response:\n', res)
-  })
-  .catch(error => {
-    console.error('Error:\n', error)
-  })
-});
+    Axios.post(TELEGRAF_HTTP_LISTENER, data, axiosRequestConfig)
+      .then(res => {
+        console.log('Response:\n', res)
+      })
+      .catch(error => {
+        console.error('Error:\n', error)
+      })
+  });
+}
+
+
 
 
 
